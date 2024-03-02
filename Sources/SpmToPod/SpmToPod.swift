5a206529348d5d4eae3cb39600b05aa6e06e35ac
= <abdullahijimoh3.ja@gmail.com>
2024-03-02 23:25:21 +0100

    Add changes

--- Sources/SpmToPod/SpmToPod.swift
+++ Sources/SpmToPod/SpmToPod.swift
@@ -1,9 +1,10 @@
 import SwiftUI
+import Foundation
 
 extension View {
     
     public func showImage() -> some View {
-        Image("my_tree")
+        Image("my_tree", bundle: .module)
             .frame(width: 200, height: 200)
     }
 }
--- Tests/SpmToPodTests/SpmToPodTests.swift
+++ Tests/SpmToPodTests/SpmToPodTests.swift
@@ -6,6 +6,6 @@ final class SpmToPodTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SpmToPod().text, "Hello, World!")
+        //XCTAssertEqual(SpmToPod().text, "Hello, World!")
     }
 }
