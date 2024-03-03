341292dedfce560bae063a54913eb8798c258372
= <abdullahijimoh3.ja@gmail.com>
2024-03-03 02:03:16 +0100

    Add license

--- Sources/SpmToPod/SpmToPod.swift
+++ Sources/SpmToPod/SpmToPod.swift
@@ -4,7 +4,34 @@ import Foundation
 extension View {
     
     public func showImage() -> some View {
-        Image("my_tree", bundle: .module)
+        Image("my_tree")
             .frame(width: 200, height: 200)
+        
     }
+}
+extension Bundle {
+    static var modulee: Bundle = {
+        #if SWIFT_PACKAGE
+        return Bundle.module
+        #else
+        return Bundle(for: SampleClass.self)
+        #endif
+    }()
+}
+
+class SampleClass {}
+class ImageHelper {
+    static var resolvedBundle: Bundle {
+        #if SWIFT_PACKAGE
+            return Bundle.module
+        #else
+            return Bundle(for: self)
+        #endif
+    }
+    
+//    static func image(_ name: String) -> UIImage? {
+//        return UIImage(named: name,
+//                       in: resolvedBundle,
+//                       compatibleWith: nil)
+//    }
 }
